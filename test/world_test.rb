c59749ca15f95af00f57718305dd18726a86b6ec
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2014-07-16 06:35:55 -0500

    Added world test minimal skeleton

---
+++ test/world_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class WorldTest < MiniTest::Unit::TestCase
+
+  def setup
+    # This is ran before all the tests. Useful
+    # when preparing input data for the objects/methods you
+    # wish to test.
+  end
+
+end
