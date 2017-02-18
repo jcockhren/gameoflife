5353062f7e724d5dee66d9b5806f72677f9dac79
Jurnell Cockhren <jurnell@sophicware.com>
2017-02-18 10:39:27 -0800

    Example of 2D Bounded world for class

--- csharp/GameOfLife/World.cs
+++ csharp/GameOfLife/World.cs
@@ -8,6 +8,22 @@ namespace GameOfLife
 {
     public class World : IGameOfLife
     {
+        int[,] contents = new int[50,50]; // Where's my origin?
+        // ^ 2-D array of zeros!! :D
+
+        public World(int[,] live_cells)
+        {
+            for (int i = 0; i < (live_cells.Length)/2; i++)
+            {
+                // i represents a live cell (position)
+                int live_cell_x_coord = live_cells[i, 0];
+                int live_cell_y_coord = live_cells[i, 1];
+
+                // It's alive!!!!!
+                contents[live_cell_x_coord, live_cell_y_coord] = 1;
+            }
+        }
+
         public void LiveOn(object input)
         {
             throw new NotImplementedException();
@@ -26,6 +42,23 @@ namespace GameOfLife
         public void UnderPopulation(object input)
         {
             throw new NotImplementedException();
+        }
+
+        public int CountLiveNighbors(int x, int y)
+        {
+            int top, bottom, left, right, top_left, top_right;
+            int bottom_left, bottom_right;
+
+            top = contents[x, y + 1];
+            bottom = contents[x, y - 1];
+            left = contents[x-1, y];
+            right = contents[x+1, y];
+            bottom_left = contents[x-1, y - 1];
+            top_left = contents[x-1, y + 1];
+            bottom_right = contents[x+1, y - 1];
+            top_right = contents[x+1,y+1];
+
+            return top + bottom + bottom_left + bottom_right + left + top_left + top_right + right;
         }
     }
 }
--- csharp/GameOfLifeTests/GameOfLifeTests.cs
+++ csharp/GameOfLifeTests/GameOfLifeTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameOfLife;
 
 namespace GameOfLifeTests
 {
@@ -38,8 +39,23 @@ namespace GameOfLifeTests
         [TestMethod]
         public void EnsureICanCountLiveNeighbors()
         {
+            /*
+            int[,] cell_holder = new int[2, 4]; //4-cells each with 2 coordinates
+            cell_holder[0, 0] = 17; // X coordinate in my World 
+            cell_holder[0, 1] = 17; // Y Coordinate in my World
+            */
+            // Same as above
+            int[,] cell_holder = { { 17, 17 }, { 17, 18 } };
 
-        }
+            // Live cell at coordinate (17,17)
+            World world = new World(cell_holder);
+            
+
+            // Act
+            int actual_count = world.CountLiveNighbors(17, 17);
+            int expected_count = 1; 
+            Assert.AreEqual(expected_count, actual_count);
+;        }
 
         [TestMethod]
         public void EnsureReproductionWithOscillator()
